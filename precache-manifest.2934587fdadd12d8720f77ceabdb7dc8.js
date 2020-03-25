16cd12c8cec1047a1e496635a17beb5fb4f55eb6
vishalibitwar <vish31ibitwar@gmail.com>
2020-03-25 11:50:16 +0530

    Updates

---
+++ precache-manifest.2934587fdadd12d8720f77ceabdb7dc8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0f19998fa32fc1c84ce47dc85e23c0",
+    "url": "/GitFinder/index.html/index.html"
+  },
+  {
+    "revision": "276076a283a5f74a5f98",
+    "url": "/GitFinder/index.html/static/css/main.10f6ddda.chunk.css"
+  },
+  {
+    "revision": "9b2a86019d9174fdcddf",
+    "url": "/GitFinder/index.html/static/js/2.a4b66560.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/GitFinder/index.html/static/js/2.a4b66560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "276076a283a5f74a5f98",
+    "url": "/GitFinder/index.html/static/js/main.c003103f.chunk.js"
+  },
+  {
+    "revision": "00677ee827fb0173c636",
+    "url": "/GitFinder/index.html/static/js/runtime-main.c4d28812.js"
+  },
+  {
+    "revision": "e47458bea6d0c2f5010db318374ad238",
+    "url": "/GitFinder/index.html/static/media/About2.e47458be.png"
+  },
+  {
+    "revision": "2348c2826c2e5385805764dfc7bdf84f",
+    "url": "/GitFinder/index.html/static/media/PageNotFound.2348c282.png"
+  },
+  {
+    "revision": "d617763fd5c08d9196374c29974e30ab",
+    "url": "/GitFinder/index.html/static/media/home.d617763f.svg"
+  }
+]);
